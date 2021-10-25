f632d88d4e79bea0ffe70ea3371e5465f930e350
FxTsuna <FxTsuna@outlook.fr>
2021-10-25 15:24:11 +0200

    UUID instead of Integer for ID Adding 1 test method for Controller A interface repo for testing

--- src/fr/umlv/main/CalendarController.java
+++ src/fr/umlv/main/CalendarController.java
@@ -2,12 +2,23 @@ package fr.umlv.main;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
 public class CalendarController {
     @Autowired
     private CalendarService calendarService;
+
+    private final BdRepo bdRepo;
+
+    public CalendarController(BdRepo bdRepo) {
+        this.bdRepo = bdRepo;
+    }
+
+    @GetMapping("/calendar")
+    public List<Calendar> getId() {
+        return bdRepo.findAll();
+    }
 }
