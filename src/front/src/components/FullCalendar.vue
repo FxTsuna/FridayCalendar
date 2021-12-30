bf3150ef57604a3695af3a15c29a9134ae8406ab
FxTsuna <FxTsuna@outlook.fr>
2021-12-31 00:33:43 +0100

    Front addEvent and onClick event

--- src/front/src/components/FullCalendar.vue
+++ src/front/src/components/FullCalendar.vue
@@ -1,34 +1,68 @@
 <template>
-  <FullCalendar :options="calendarOptions" />
+  <FullCalendar :options="calendarOptions"/>
 </template>
 
 <script>
-import '@fullcalendar/core/vdom' // solves problem with Vite
+//import { reactive } from "vue";
+//import '@fullcalendar/core/vdom' // solves problem with Vite
 import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction'
+import router from "@/router";
+//import EventModal from './ModalComponent'
 
 export default {
   components: {
     FullCalendar // make the <FullCalendar> tag available
   },
-  data() {
-    return {
+  data: () => ({
       calendarOptions: {
         plugins: [ dayGridPlugin, interactionPlugin ],
         initialView: 'dayGridMonth',
-        dateClick: this.handleDateClick,
+        editable: true,
+        selectable: true,
+        select: (arg) => {
+
+          console.log(arg.start + ' to ' + arg.end)
+/*
+          fetch("/event/save", {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({title: "default event name", dateStart: arg.start, dateEnd: arg.end, info: "default info"})
+          }).then((res => {
+            if (res.status === 201) {
+              fetch("event/get l'id somehow et le mettre dans event")
+            }
+          }))
+
+
+ */
+          const cal = arg.view.calendar
+          cal.unselect()
+          cal.addEvent({
+            id: "",
+            title:"ui",
+            start: arg.start,
+            end: arg.end,
+            allDay: true
+          })
+
+        },
+
+        eventClick: (arg) => {
+          console.log(arg)
+          const event = JSON.stringify(arg.event);
+          localStorage.setItem('event', event)
+          router.push('EventModify')
+        },
+
         events: [
-          { title: 'event 1', start: '2022-01-01' },
+          { title: 'event 1', start: '2022-01-01', end: '2022-01-05' },
           { title: 'event 2', start: '2022-01-02' }
-        ]
-      }
-    }
-  },
+        ]},
+
+  }),
   methods: {
-    handleDateClick: function(arg) {
-      alert('date click! ' + arg.dateStr)
-    }
   }
 }
 </script>
